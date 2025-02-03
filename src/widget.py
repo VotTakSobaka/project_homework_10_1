7f236b1e7cb5463a4878a8b669323f99fa427b9b
Igor Igor <smeshnoy111@icloud.com>
2025-02-04 00:30:03 +0300

    добавлена функция get_date, и внесены правки(black,isort)

--- src/widget.py
+++ src/widget.py
@@ -8,7 +8,7 @@ def mask_account_card(info: str) -> str:
     """
     # Разделяем строку на тип и номер
     parts = info.split()
-    card_type = ' '.join(parts[:-1])  # Все части, кроме последней, составляют тип
+    card_type = " ".join(parts[:-1])  # Все части, кроме последней, составляют тип
     number = parts[-1]  # Последняя часть - это номер
 
     # Проверяем, является ли номер картой или счетом
@@ -29,6 +29,29 @@ def mask_account_card(info: str) -> str:
     else:
         return "Ошибка: неизвестный тип карты или счета."
 
+
 input_info = input("Введите тип и номер карты или счета: ")
 masked_result = mask_account_card(input_info)
 print(masked_result)
+
+
+def get_date(date_str: str) -> str:
+    """
+    Преобразовать строку с датой в формат "ДД.ММ.ГГГГ".
+
+    Функция принимает строку, содержащую дату в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ".
+
+    """
+    # Разделяем строку на дату и время
+    date_part = date_str.split("T")[0]
+    # Разделяем дату на год, месяц и день
+    year, month, day = date_part.split("-")
+    # Формируем строку в нужном формате
+    formatted_date = f"{day}.{month}.{year}"
+    return formatted_date
+
+
+input_date = input("Введите дату в формате 'YYYY-MM-DDTHH:MM:SS.ssssss': ")
+formatted_result = get_date(input_date)
+print("Дата в формате 'ДД.ММ.ГГГГ':", formatted_result)
