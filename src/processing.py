9f6e6a45c19fa75d4c8f53f97f5d8668f242a04e
Igor Igor <smeshnoy111@icloud.com>
2025-02-16 23:02:35 +0300

    installation poetry/black/mypy/isort and fixed processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(data, state='EXECUTED'):
+def filter_by_state(data, state="EXECUTED"):
     """
     Фильтрует список словарей по значению ключа 'state'.
 
@@ -7,17 +7,39 @@ def filter_by_state(data, state='EXECUTED'):
     указанному значению.
 
     """
-    return [item for item in data if item.get('state') == state]
+    return [item for item in data if item.get("state") == state]
+
 
 input_data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
 filtered_result_executed = filter_by_state(input_data)
 print(filtered_result_executed)
 
-filtered_result_canceled = filter_by_state(input_data, 'CANCELED')
+filtered_result_canceled = filter_by_state(input_data, "CANCELED")
 print(filtered_result_canceled)
+
+
+def sort_by_date(data, reverse: bool = True) -> list:
+    """
+    Сортирует список словарей по значению ключа 'date'.
+
+    Функция принимает список словарей и возвращает новый список,
+    отсортированный по дате.
+
+    """
+    return sorted(data, key=lambda x: x["date"], reverse=reverse)
+
+
+input_data = [{"id": 1, "date": "2023-01-01"}, {"id": 2, "date": "2022-12-31"}, {"id": 3, "date": "2023-01-02"}]
+
+sorted_result_desc = sort_by_date(input_data)
+print(sorted_result_desc)
+
+# Сортировка по дате в порядке возрастания
+sorted_result_asc = sort_by_date(input_data, reverse=False)
+print(sorted_result_asc)
